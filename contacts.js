ddd6355b2039ec3e5526ffdbd3f7dc56544008a6
Vladislav Kulachko <vladcoool@gmail.com>
2021-11-10 13:06:51 +0200

    заканчивает CLI

--- contacts.js
+++ contacts.js
@@ -5,30 +5,27 @@ import { randomUUID } from "crypto";
 const contactsPath = pathToFileURL("./db/contacts.json");
 
 export async function listContacts() {
-  const data = await readFile(contactsPath, "utf8");
+  const data = await readFile(contactsPath);
   return JSON.parse(data);
 }
 
 export async function getContactById(contactId) {
-  const data = await readFile(contactsPath, "utf8");
-  const oneContact = JSON.parse(data).find(
-    ({ id }) => String(id) === contactId
-  );
+  const contacts = await listContacts();
+  const oneContact = contacts.find(({ id }) => String(id) === contactId);
   return oneContact;
 }
 
 export async function removeContact(contactId) {
-  const data = await readFile(contactsPath, "utf8");
-  const newList = JSON.parse(data).filter(({ id }) => String(id) !== contactId);
-  await writeFile(contactsPath, JSON.stringify(newList, null, 2), "utf8");
+  const contacts = await listContacts();
+  const newList = contacts.filter(({ id }) => String(id) !== contactId);
+  await writeFile(contactsPath, JSON.stringify(newList, null, 2));
   return newList;
 }
 
 export async function addContact(name, email, phone) {
   const newContact = { id: randomUUID(), name, email, phone };
-  const data = await readFile(contactsPath, "utf8");
-  const parsedData = JSON.parse(data);
-  parsedData.push(newContact);
-  await writeFile(contactsPath, JSON.stringify(parsedData, null, 2), "utf8");
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
